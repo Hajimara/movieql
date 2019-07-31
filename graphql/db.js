b4c90103c2abb1001574e2b77e5aab56859ebbc8
park sujin <show3rg@gmail.com>
2019-07-31 14:27:33 +0900

    API init

--- graphql/db.js
+++ graphql/db.js
@@ -1,38 +1,69 @@
-export const people = [
-	{
-		id : "0",
-		name : "sujin",
-		age : "27",
-		gender : "men"
-	},{
-		id : "1",
-		name : "sujin",
-		age : "27",
-		gender : "men"
-	},{
-		id : "2",
-		name : "sujin",
-		age : "27",
-		gender : "men"
-	},{
-		id : "3",
-		name : "sujin",
-		age : "27",
-		gender : "men"
-	},{
-		id : "4",
-		name : "sujin",
-		age : "27",
-		gender : "men"
-	},{
-		id : "5",
-		name : "sujin",
-		age : "27",
-		gender : "men"
-	},
-];
+import fetch from "node-fetch";
+const API_URL = "https://yts.lt/api/v2/list_movies.json?";
 
-export const getById = id => {
-	const filteredMoviesPeople = people.filter(person => String(id) === person.id);
-	return filteredMoviesPeople[0];
+export const getMovies = (limit, rating) => {
+	let REQUEST_URL = API_URL;
+	if(limit > 0){
+		REQUEST_URL += `limit=${limit}`;
+	}
+	if(rating > 0){
+		REQUEST_URL += `&minimum_rating=${rating}`;
+	}
+	return fetch(REQUEST_URL)
+	.then(res => res.json())
+	.then(json => json.data.movies);
 }
+
+// fetch or axios
+
+	// export const getMovies = (limit, rating) =>
+	// fetch(`${API_URL}`)
+	// .then(res => res.json())
+	// .then(json => json.data.movies);
+
+// export let movies = [
+// 	{
+// 		id : 0,
+// 		name : "엑시트",
+// 		score : 1
+// 	},{
+// 		id : 1,
+// 		name : "라이온 킹",
+// 		score : 12
+// 	},{
+// 		id : 2,
+// 		name : "스파이더맨-파 프롬 홈",
+// 		score : 13
+// 	},{
+// 		id : 3,
+// 		name : "레드슈즈",
+// 		score : 121
+// 	}
+// ];
+
+// export const getMovies = () => movies;
+
+// export const getById = id => {
+// 	const filteredMovies = movies.filter(movie => id === movie.id);
+// 	return filteredMovies[0]; 
+// }
+
+// export const deleteMovie = (id) =>{
+// 	const cleanedMovies = movies.filter(movie => movie.id !== id);
+// 	if(movies.length > cleanedMovies.length) {
+// 		movies = cleanedMovies;
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// };
+
+// export const addMovie = (name, score) => {
+// 	const newMovie = {
+// 		id: `${movies.length + 1}`,
+// 		name,
+// 		score
+// 	}
+// 	movies.push(newMovie);
+// 	return newMovie;
+// }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,25 @@
-import { people, getById } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
 	Query: {
-		people: () => people,
-		person: (_, { id }) => getById(id)
+		movies: (_,{rating, limit}) => getMovies(limit, rating)
 	}
 };
 
 export default resolvers;
+
+
+// import { addMovie, getMovies, getById, deleteMovie } from "./db";
+
+// const resolvers = {
+// 	Query: {
+// 		movies: () => getMovies(),
+// 		movie: (_, { id }) => getById(id)
+// 	},
+// 	Mutation: {
+// 		addMovie: (_, { name, score }) => addMovie(name, score),
+// 		deleteMovie: (_,{id}) => deleteMovie(id)
+// 	}
+// };
+
+// export default resolvers;
